2a087e81b2ea1fd53d1439e0dee86e5b8d1800a4
aaml-analytics <58523582+aaml-analytics@users.noreply.github.com>
2020-06-29 13:04:59 +0100

    Update app.py

--- app.py
+++ app.py
@@ -30,7 +30,8 @@ import threading
 threading._DummyThread._Thread__stop = lambda x: 42
 
 external_stylesheets = [dbc.themes.BOOTSTRAP, 'https://codepen.io/chriddyp/pen/bWLwgP.css',
-                        "https://codepen.io/sutharson/pen/ZEbqopm.css"]
+                        "https://codepen.io/sutharson/pen/ZEbqopm.css", 
+                        "https://fonts.googleapis.com/css2?family=Raleway&display=swap"]
 
 SUP = str.maketrans("0123456789", "⁰¹²³⁴⁵⁶⁷⁸⁹")
 
